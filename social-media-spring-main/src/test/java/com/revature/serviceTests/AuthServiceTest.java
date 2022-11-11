52b7bd49c571df47a82279bb52b2e254bf8f8d09
d-rivera3 <rivera.daniel619@gmail.com>
2022-11-10 20:24:44 -0700

    h2 application.yml is good, testing if AuthService.test is an issue

--- social-media-spring-main/src/test/java/com/revature/serviceTests/AuthServiceTest.java
+++ social-media-spring-main/src/test/java/com/revature/serviceTests/AuthServiceTest.java
@@ -1,55 +1,55 @@
-// package com.revature.serviceTests;
+package com.revature.serviceTests;
 
-// import com.revature.exceptions.InvalidInputException;
-// import com.revature.models.User;
-// import com.revature.services.AuthService;
-// import com.revature.services.UserService;
+import com.revature.exceptions.InvalidInputException;
+import com.revature.models.User;
+import com.revature.services.AuthService;
+import com.revature.services.UserService;
 
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;
 
-// @SpringBootTest
-// public class AuthServiceTest {
+@SpringBootTest
+public class AuthServiceTest {
 
-//     @Mock
-//     UserService userService;
+    @Mock
+    UserService userService;
 
-//     @InjectMocks
-//     AuthService as;
+    @InjectMocks
+    AuthService as;
 
-//     @Test
-//     public void register_INPUT_User_Expect_SameUser(){
-//         try {
-//             User tmpUser = new User();
-//             when(userService.save(tmpUser)).thenReturn(tmpUser);
+    @Test
+    public void register_INPUT_User_Expect_SameUser(){
+        try {
+            User tmpUser = new User();
+            when(userService.save(tmpUser)).thenReturn(tmpUser);
 
-//             Assertions.assertEquals(tmpUser, as.register(tmpUser));
-//         } catch (InvalidInputException e){
-//             System.out.println(e.getMessage());
-//         }
-//     }
+            Assertions.assertEquals(tmpUser, as.register(tmpUser));
+        } catch (InvalidInputException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
-//     @Test
-//     public void findByCredentials_INPUT_validCredentials_EXPECT_userOptional(){
-//         User tmpUser = new User(1, "test@test.com", "password", "Testy", "McTestface");
-//         when(userService.findByCredentials(tmpUser.getEmail(), tmpUser.getPassword())).thenReturn( Optional.of( tmpUser ) );
+    @Test
+    public void findByCredentials_INPUT_validCredentials_EXPECT_userOptional(){
+        User tmpUser = new User(1, "test@test.com", "password", "Testy", "McTestface");
+        when(userService.findByCredentials(tmpUser.getEmail(), tmpUser.getPassword())).thenReturn( Optional.of( tmpUser ) );
 
-//         Assertions.assertEquals(Optional.of( tmpUser ), as.findByCredentials(tmpUser.getEmail(), tmpUser.getPassword()));
-//     }
+        Assertions.assertEquals(Optional.of( tmpUser ), as.findByCredentials(tmpUser.getEmail(), tmpUser.getPassword()));
+    }
 
-//     @Test
-//     public void findByCredentials_INPUT_invalidCredentials_EXPECT_emptyOptional(){
-//         User tmpUser = new User(1, "test@test.com", "password", "Testy", "McTestface");
-//         when(userService.findByCredentials("est@test.com", tmpUser.getPassword())).thenReturn( Optional.empty( ) );
+    @Test
+    public void findByCredentials_INPUT_invalidCredentials_EXPECT_emptyOptional(){
+        User tmpUser = new User(1, "test@test.com", "password", "Testy", "McTestface");
+        when(userService.findByCredentials("est@test.com", tmpUser.getPassword())).thenReturn( Optional.empty( ) );
 
-//         Assertions.assertEquals(Optional.empty( ), as.findByCredentials(tmpUser.getEmail(), tmpUser.getPassword()));
-//     }
+        Assertions.assertEquals(Optional.empty( ), as.findByCredentials(tmpUser.getEmail(), tmpUser.getPassword()));
+    }
 
-// }
+}
