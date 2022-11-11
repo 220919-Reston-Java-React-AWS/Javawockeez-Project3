6b3f7a919345219dc20ae1e106fad2ba7f6bd31c
d-rivera3 <rivera.daniel619@gmail.com>
2022-11-10 20:29:43 -0700

    AuthService.test is ok, test if PostServiceTest is an issue

--- social-media-spring-main/src/test/java/com/revature/serviceTests/PostServiceTest.java
+++ social-media-spring-main/src/test/java/com/revature/serviceTests/PostServiceTest.java
@@ -1,31 +1,31 @@
-// package com.revature.serviceTests;
+package com.revature.serviceTests;
 
 
-// import com.revature.models.Post;
-// import com.revature.repositories.PostRepository;
-// import com.revature.services.PostService;
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.springframework.boot.test.context.SpringBootTest;
+import com.revature.models.Post;
+import com.revature.repositories.PostRepository;
+import com.revature.services.PostService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;
 
-// @SpringBootTest
-// public class PostServiceTest {
+@SpringBootTest
+public class PostServiceTest {
 
-//     @Mock
-//     PostRepository postRepository;
+    @Mock
+    PostRepository postRepository;
 
-//     @InjectMocks
-//     PostService ps;
+    @InjectMocks
+    PostService ps;
 
-//     @Test
-//     public void upsert_INSERT_post_EXPECT_post(){
-//         Post tmpPost = new Post();
-//         when(postRepository.save(tmpPost)).thenReturn(tmpPost);
+    @Test
+    public void upsert_INSERT_post_EXPECT_post(){
+        Post tmpPost = new Post();
+        when(postRepository.save(tmpPost)).thenReturn(tmpPost);
 
-//         Assertions.assertEquals(tmpPost, ps.upsert(tmpPost));
-//     }
-// }
+        Assertions.assertEquals(tmpPost, ps.upsert(tmpPost));
+    }
+}
