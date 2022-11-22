396f975fd9896f05ce8ac4989f07fd1a44ad2e08
d-rivera3 <rivera.daniel619@gmail.com>
2022-11-21 17:14:05 -0700

    fixing samplequestion1 test file

--- social-media-spring-main/src/test/java/com/revature/serviceTests/SampleQuestions1ServiceTest.java
+++ social-media-spring-main/src/test/java/com/revature/serviceTests/SampleQuestions1ServiceTest.java
@@ -1,52 +1,48 @@
 package com.revature.serviceTests;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import com.revature.models.SampleQuestions1;
 import com.revature.repositories.SampleQuestions1Repository;
+
 import com.revature.services.SampleQuestions1Service;
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class SampleQuestions1ServiceTest {
-
     @Mock
-    SampleQuestions1Repository s1;
-
+    SampleQuestions1Repository sampleQuestions1Repository;
     @InjectMocks
-    SampleQuestions1Service s1Service;
+    SampleQuestions1Service sampleQuestions1Service;
 
-    SampleQuestions1 question1;
+    /*-------Arranged Values for Tests------*/
+    SampleQuestions1 testQuestion1;
+    List<SampleQuestions1> testQuestionList;
 
-    SampleQuestions1 question2;
-
-    List<SampleQuestions1> allQuestions;
-    
     @BeforeEach
-    public void initTests(){
-        this.s1Service = new SampleQuestions1Service(this.s1);
-        this.question1 = new SampleQuestions1(1, "Am I the first test question??");
-        this.allQuestions = Arrays.asList(this.question1, this.question2);
+    void initTestValues(){
+        // set question
+        this.testQuestion1 = new SampleQuestions1(1, "test question");
+        this.testQuestionList = Arrays.asList(testQuestion1);
     }
 
-    @Test
-    public void listAllQuestions(){
-        System.out.println("Test 1");
-        
-        when(this.s1Service.listQuestions()).thenReturn( allQuestions );
 
-        Assertions.assertEquals( allQuestions, s1Service.listQuestions() );
+    /*------List<SampleQuestions1> listQuestions() Tests------*/
+    @Test
+    public void listQuestion_questionFound(){
+        try{
+            when(sampleQuestions1Service.listQuestions()).thenReturn(testQuestionList);
+
+            Assertions.assertEquals(testQuestionList, sampleQuestions1Service.listQuestions());
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
